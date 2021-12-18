2b8b6c6e16e29f738824e26f72adeddc04a2cd58
Kirill <k261252@yandex.ru>
2021-12-18 23:00:41 +0700

    18

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -41,3 +41,10 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000,
                                help_text='Введите краткое описание книги',
                                verbose_name='Аннотация')
+    isbn = models.CharField(max_length=13, help_text="должно содержать 13 символов",
+                            verbose_name="ISBN Книги")
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url (self):
+        return reverse('book-detail', args=[str(self.id)])
