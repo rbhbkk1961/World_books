0cf52431bfacd0e40d6e3e60d6ec9fdbfc064bad
Kirill <k261252@yandex.ru>
2022-02-02 16:52:02 +0700

    dfdf

--- WebBooks/catalog/models.py
+++ WebBooks/catalog/models.py
@@ -19,10 +19,15 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100,
                                  help_text='Введите фамилию автора',
                                  verbose_name='Фамилия автора')
-    date_of_birth = models.DataField(
+    date_of_birth = models.DateField(
                                   help_text='Введите дату рождения',
                                   verbose_name='Дата рождения',
                                   null=True, blank=True)
+    date_of_death = models.DateField(
+                                help_text='Введите дату смерти',
+                                verbose_name='Дата смерти',
+                                null=True, blank=True)
+
     def __str__(self):
         return self.last_name
 class Book(models.Model):
@@ -32,12 +37,12 @@ class Book(models.Model):
     genre = models.ForeignKey('Genre', on_delete=models.CASCADE,
                               help_text='Выберите жанр книги',
                               verbose_name='Жанр книги', null=True)
-    laguage = models.ForeignKey('language', on_delete=models.CASCADE,
+    laguage = models.ForeignKey('Language', on_delete=models.CASCADE,
                                 help_text='Выберите язык книги',
                                 verbose_name='Язык книги', null=True)
     author = models.ManyToManyField('Author',
                                     help_text='Выберите автора  книги',
-                                    verbose_name='Автор книги', null=True)
+                                    verbose_name='Автор книги')
     summary = models.TextField(max_length=1000,
                                help_text='Введите краткое описание книги',
                                verbose_name='Аннотация')
@@ -46,5 +51,28 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
-    def get_absolute_url (self):
+    def get_absolute_url(self):
         return reverse('book-detail', args=[str(self.id)])
+class Status(models.Model):
+    name = models.CharField(max_length=20,
+                            help_text="Введите статус экземпляра книги",
+                            verbose_name="Статус экземпляра книги")
+    def __str__(self):
+        return self.name
+class BookInstance(models.Model):
+    book = models.ForeignKey('Book', on_delete=models.CASCADE, null=True)
+    inv_nom = models.CharField(max_length=20, null=True,
+                               help_text="Введите инвентарный номер",
+                               verbose_name="Инвентарный номер")
+    imprint = models.CharField(max_length=200,
+                               help_text="Введите издательство и год выпуска",
+                               verbose_name="Издательство")
+    status = models.ForeignKey('Status', on_delete=models.CASCADE,
+                               null=True,
+                               help_text='Изменить состояние экземпляра',
+                               verbose_name="Статус экземпляра книги")
+    due_back = models.DateField(null=True, blank=True,
+                                help_text="Введите конец срока статуса",
+                                verbose_name="Дата окончания статуса")
+    def __str__(self):
+        return '%s %s %s' % (self.inv_nom,self.book,self.status)
