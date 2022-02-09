d7907e659d68ca7f3c76769a465c01d34a38b151
Kirill <k261252@yandex.ru>
2022-02-09 15:24:15 +0700

    322

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
 from .models import Author, Book, Genre, Language, Status, BookInstance
-admin.site.register(Author)
-admin.site.register(Book)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+admin.site.register(Author, AuthorAdmin)
+#admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Language)
 admin.site.register(Status)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title','genre','laguage','display_author')
+    list_filter = ('genre','author')
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('book', 'status')
+
