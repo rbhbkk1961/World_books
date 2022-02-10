a8fa61607e93ad71080a057c8e5a69a9e24f74db
Kirill <k261252@yandex.ru>
2022-02-10 18:16:04 +0700

    330

--- WebBooks/catalog/admin.py
+++ WebBooks/catalog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from .models import Author, Book, Genre, Language, Status, BookInstance
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    list_display = ('last_name', 'first_name')
+    fields = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 admin.site.register(Author, AuthorAdmin)
 #admin.site.register(Book)
 admin.site.register(Genre)
@@ -9,12 +10,24 @@ admin.site.register(Language)
 admin.site.register(Status)
 #admin.site.register(BookInstance)
 
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title','genre','laguage','display_author')
     list_filter = ('genre','author')
+    inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('book', 'status')
+    fieldsets = (
+        ('Экземпляр книги', {'fields':('book','imprint', 'inv_nom')
+                             }),
+        ('Статус и окончание его действия', {
+            'fields': ('status', 'due_back')
+        }),
+    )
 
