e46aee654433370370cd23bde47a69ff90acd8b4
Kirill <k261252@yandex.ru>
2022-02-10 20:21:29 +0700

    331

--- WebBooks/catalog/views.py
+++ WebBooks/catalog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import BookInstance, Book, Author, Genre
 
 def index(request):
-    return HttpResponse("Главная страница сайта Мир Книг")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    num_authors = Author.objects.count()
+
+    return render(request, 'index.html',
+                  context={'num_books':num_books,
+                           'num_instances':num_instances,
+                           'num_instances_available': num_instances_available,
+                           'num_authors' : num_authors,
+                           'num_visits': num_visits},
+                  )
+
 
 # Create your views here.
